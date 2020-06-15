31787b76be722d6f387d84643b8d45557ef46278
Nhat Quang Tran <ilovenhat230297@gmail.com>
2020-06-15 09:46:30 +0300

    hello

--- lung/app/patients/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-blueprint = Blueprint(
-    'patients_blueprint',
-    __name__,
-    url_prefix='/patients',
-    template_folder='templates',
-    static_folder='static'
-)
